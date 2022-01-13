20ca9cfcfca521e5b675a0003239b1bc0eb0478c
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 03:35:49 +0000

    S1-Example: Simple Builld

--- WhileStatement/Program.cs
+++ WhileStatement/Program.cs
@@ -10,6 +10,16 @@ namespace WhileStatement
     {
         static void Main(string[] args)
         {
+            /*This is a very simple While Statement, it evaluates the variable num. If num is less than 20 (which it is) then increment.
+             */
+            int num;
+            num = 1;
+
+            while (num < 20)
+            {
+                Console.WriteLine(num++);
+            }
+            Console.ReadLine();
         }
     }
 }
