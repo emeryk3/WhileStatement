2e4b64e845c0cd79ea62aab27c50b61060c894dd
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 03:44:35 +0000

    S1-Example: Breaking from loops
    Includes addition of a basic text file to read from.

--- WhileStatement/Program.cs
+++ WhileStatement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WhileStatement
 {
@@ -12,6 +13,8 @@ namespace WhileStatement
         {
             /*This is a very simple While Statement, it evaluates the variable num. If num is less than 20 (which it is) then increment.
              */
+
+            /*
             int num;
             num = 1;
 
@@ -20,6 +23,22 @@ namespace WhileStatement
                 Console.WriteLine(num++);
             }
             Console.ReadLine();
+            */
+
+            /* Breaking from Loops / Reading a text file:
+             * It is easy to forget to break from loops, meaning the While Statement goes on and on. When breaking from loops you use the break keyword. Create a text file and save it on your desktop; make sure it has a few lines of text, a paragraph is fine. Then at the top where the using directives are, add the System.IO namespace: */
+
+            string path = @"C:\Users\kizwi\source\repos\WhileStatement\c sharp test.txt";
+            StreamReader sr = new StreamReader(path);
+
+            string line = sr.ReadToEnd();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                break;
+            }
+            Console.ReadLine();
+
         }
     }
 }
